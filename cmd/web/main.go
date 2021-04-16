f946f4428a858468b2dc54fb8b951595342f91f8
bennycio <benny@bennyc.io>
2021-04-16 13:26:54 -0700

    Combine API and Web

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 	"net/http"
 
+	bundle "github.com/bennycio/bundle/internal"
 	"github.com/bennycio/bundle/internal/web"
 )
 
 func main() {
 	fmt.Println("Started server on port 8080")
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/", web.HandleRoot)
-	http.HandleFunc("/signup", web.HandleSignup)
-	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("../../assets/public"))))
+	bundleHandler := http.HandlerFunc(web.BundleHandlerFunc)
+	userHandler := http.HandlerFunc(web.UserHandlerFunc)
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Started server on port 8070")
+	mux.Handle("/bundle", bundle.AuthUser(bundleHandler))
+	mux.Handle("/users", bundle.AuthClient(userHandler))
+	mux.HandleFunc("/", web.HandleRoot)
+	mux.HandleFunc("/signup", web.HandleSignup)
+	mux.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("../../assets/public"))))
+
+	http.ListenAndServe(":8080", mux)
 }
